51c122949f1a6047044f94f4a487f7b27a945261
LT5B <engeleditorfpe@gmail.com>
2026-09-21 21:24:18 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true; do
+    clear
+    curl https://raw.githubusercontent.com/LT5B/hoanhegay/refs/heads/main/frames/hoan
+    sleep 1
+    curl https://raw.githubusercontent.com/LT5B/hoanhegay/refs/heads/main/frames/he
+    sleep 1
+    curl https://raw.githubusercontent.com/LT5B/hoanhegay/refs/heads/main/frames/gay
+    sleep 1
+done
