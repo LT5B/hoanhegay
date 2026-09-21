b62d007f04ac79352f4bcaf8d63114642a24c95f
LT5B <engeleditorfpe@gmail.com>
2026-09-21 21:25:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,12 @@
 while true; do
     clear
     curl https://raw.githubusercontent.com/LT5B/hoanhegay/refs/heads/main/frames/hoan
+    echo ""
     sleep 1
     curl https://raw.githubusercontent.com/LT5B/hoanhegay/refs/heads/main/frames/he
+    echo ""
     sleep 1
     curl https://raw.githubusercontent.com/LT5B/hoanhegay/refs/heads/main/frames/gay
+    echo ""
     sleep 1
 done
