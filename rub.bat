14840124512e3e1d0a6368fc990de7b1edbe7028
LT5B <engeleditorfpe@gmail.com>
2026-09-21 21:26:14 +0700

    Create rub.bat

---
+++ rub.bat
@@ -0,0 +1,13 @@
+@echo off
+:loop
+cls
+curl https://raw.githubusercontent.com/LT5B/hoanhegay/refs/heads/main/frames/hoan
+echo.
+timeout /t 1 >nul
+curl https://raw.githubusercontent.com/LT5B/hoanhegay/refs/heads/main/frames/he
+echo.
+timeout /t 1 >nul
+curl https://raw.githubusercontent.com/LT5B/hoanhegay/refs/heads/main/frames/gay
+echo.
+timeout /t 1 >nul
+goto loop
